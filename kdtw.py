1caac2bbb005f6c7b2ab5883a6ee705e33fda81f
Pierre-François Marteau <pierrefrancois.marteau@gmail.com>
2020-10-10 10:38:16 +0200

    Update kdtw.py

--- kdtw.py
+++ kdtw.py
@@ -6,7 +6,7 @@ Created on Sun Sep 27 16:46:45 2020
 """
 import numpy as np
 # Filename: kdtw.py
-# Python source code for the "Kernelized" Dynamic Time Warping sililarity (as defined in the reference below).
+# Python source code for the "Kernelized" Dynamic Time Warping similarity (as defined in the reference below).
 # Author: Pierre-Francois Marteau
 # Version: V1.0 du 13/09/2014, 
 # Licence: GPL
