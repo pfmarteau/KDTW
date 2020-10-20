f58b8c08bf16bebff24d79fc9c1886a1c4941535
Pierre-François Marteau <pierrefrancois.marteau@gmail.com>
2020-10-20 15:10:38 +0200

    Add files via upload

---
+++ KDTW_c/test.c
@@ -0,0 +1,20 @@
+static PyObject * hello_wrapper(PyObject * self, PyObject * args)
+{
+  char * input;
+  char * result;
+  PyObject * ret;
+
+  // parse arguments
+  if (!PyArg_ParseTuple(args, "s", &input)) {
+    return NULL;
+  }
+
+  // run the actual function
+  result = hello(input);
+
+  // build the resulting string into a Python object.
+  ret = PyString_FromString(result);
+  free(result);
+
+  return ret;
+}
