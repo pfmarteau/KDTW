25ae484f13d697eaea68f0146e47c4c15d787c28
Pierre-François Marteau <pierrefrancois.marteau@gmail.com>
2025-10-16 17:45:37 +0200

    Enhance setup.py with package details and requirements
    Updated setup.py to include package metadata and dependencies.

--- KDTW_c/setup.py
+++ KDTW_c/setup.py
@@ -4,5 +4,19 @@ import numpy
 KDTW = Extension('KDTW', sources=['KDTW.c'])
 
 # run the setup
-setup(ext_modules=[KDTW])
+#setup(ext_modules=[KDTW])
+setup(
+    name='kdtw',
+    version='1.0',
+    description='Positive Definite Kernel close to the Dynamic Time Warping measure',
+    author='Pierre-François Marteau',
+    author_email='pierrefrancois.marteau@gmail.com',
+    license='MIT License',
+    packages=['kdtw'],
+    install_requires=['numpy'],
+    ext_modules=[
+        Extension("KDTW", ["KDTW.c"],
+                  include_dirs=[numpy.get_include()]),
+    ],
+)
 
